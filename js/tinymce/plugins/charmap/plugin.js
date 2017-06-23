eb228216ca9197111e2faeb24e155e47c4fc0bc2
Korn <NChanwimalueng@dstworldwideservices.com>
2017-06-23 16:00:18 +0700

    backported TINY-909

--- js/tinymce/plugins/charmap/plugin.js
+++ js/tinymce/plugins/charmap/plugin.js
@@ -378,8 +378,9 @@ tinymce.PluginManager.add('charmap', function(editor) {
 				var target = e.target;
 
 				if (/^(TD|DIV)$/.test(target.nodeName)) {
-					if (getParentTd(target).firstChild) {
-						insertChar(target.getAttribute('data-chr'));
+					var charDiv = getParentTd(target).firstChild;
+						if (charDiv && charDiv.hasAttribute('data-chr')) {
+							insertChar(charDiv.getAttribute('data-chr'));
 
 						if (!e.ctrlKey) {
 							win.close();
